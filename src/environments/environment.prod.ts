0ea1ad9fe9b98a7083f4f69fd3cb95a7ce0ee849
Marijan <mazrins@outlook.com>
2020-06-30 08:46:10 +0200

    adding firebaseConfig to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBJUWMLRk0CSNON2iHzB_ilAY3s1Bd2jJ0',
+    authDomain: 'evidencija-radnika-8d784.firebaseapp.com',
+    databaseURL: 'https://evidencija-radnika-8d784.firebaseio.com',
+    projectId: 'evidencija-radnika-8d784',
+    storageBucket: 'evidencija-radnika-8d784.appspot.com',
+    messagingSenderId: '780812564088',
+    appId: '1:780812564088:web:e8bb16eeadf31770cb5f8d',
+    measurementId: 'G-SKJYGWXBNJ',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBJUWMLRk0CSNON2iHzB_ilAY3s1Bd2jJ0',
+    authDomain: 'evidencija-radnika-8d784.firebaseapp.com',
+    databaseURL: 'https://evidencija-radnika-8d784.firebaseio.com',
+    projectId: 'evidencija-radnika-8d784',
+    storageBucket: 'evidencija-radnika-8d784.appspot.com',
+    messagingSenderId: '780812564088',
+    appId: '1:780812564088:web:e8bb16eeadf31770cb5f8d',
+    measurementId: 'G-SKJYGWXBNJ',
+  },
 };
 
 /*
