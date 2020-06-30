a174e48ff2c2b76f4e03321262a68c87953f26a5
Marijan <mazrins@outlook.com>
2020-06-30 17:04:06 +0200

    profile component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, ProfileComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
